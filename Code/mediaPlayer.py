b77881b86772f4eb38c0b11eda7580518d9ad66b
Alex Sanchez <alexsanchez@Alexs-MacBook-Air.local>
2020-11-06 07:22:06 -0500

    update

--- Code/mediaPlayer.py
+++ Code/mediaPlayer.py
@@ -107,8 +107,8 @@ colorFrames = queueThread()
 grayFrames = queueThread()
 
 extraceT = threading.Thread(target = extractFrames, args = (clipFileName, colorFrames))
-convertT = threading.Thread(target = extractFrames, args = (grayFrames, colorFrames))
-displayT = threading.Thread(target = displayFrames, args = (grayFrames))
+convertT = threading.Thread(target = convertToGray, args = (grayFrames, colorFrames))
+displayT = threading.Thread(target = displayFrames, args = (grayFrames,))
 
 extraceT.start()
 convertT.start()
