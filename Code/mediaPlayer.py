cad73003f5ee4fba82b0358975761c108529aef7
Alex Sanchez <alexsanchez@Alexs-MacBook-Air.local>
2020-11-11 21:08:53 -0500

    test

--- Code/mediaPlayer.py
+++ Code/mediaPlayer.py
@@ -46,7 +46,7 @@ def displayFrames(grayFrames):
     # load the first gray frame
     frame = grayFrames.dequeue() 
 
-    while frame is not None:
+    while frame != '!':
         print(f'Displaying frame {count}')
 
         # Decode back the frame into an image
